94560d933ca51f303fe49c5fde3041b7ccf42f4c
chengjun <wangchj04@gmail.com>
2018-02-12 16:42:20 +0800

    rebuilding site

--- deploy_raw_files_to_github.sh
+++ deploy_raw_files_to_github.sh
@@ -2,7 +2,7 @@
 
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
-git config 'lfs.https://github.com/data-science-lab/data-science-lab.github.io.git/info/lfs.locksverify' false
+#git config 'lfs.https://github.com/data-science-lab/data-science-lab.github.io.git/info/lfs.locksverify' false
 
 # git pull first
 #git pull origin master
