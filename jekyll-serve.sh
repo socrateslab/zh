0c4f1b23ef6b4da69c7335694c5d1e0330ba11a8
chengjun <wangchj04@gmail.com>
2019-10-08 18:13:43 +0800

    rebuilding site

--- jekyll-serve.sh
+++ jekyll-serve.sh
@@ -1,2 +1,7 @@
-jekyll serve # --incremental
+bundle exec jekyll serve # --incremental
 #jekyll build --trace
+# a. 使用命令：
+# ps aux | grep jekyll 查找到进程号
+
+# 　　b. 使用命令：
+# kill -9 28185 杀死进程（其中28185是进程号）
